ff593c8bc820f28e9409f58c2fffb584b73adbde
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-21 23:48:31 +0545

    Ui : Login page sign with google added

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -6,12 +6,13 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              Image.asset("assets/images/devanasoft_logo.png", height: 80),
+              const SizedBox(height: 20),
+              Image.asset("assets/images/devanasoft_logos.png", height: 120),
 
               const SizedBox(height: 10),
 
@@ -24,6 +25,51 @@ class LoginPage extends StatelessWidget {
                   color: Colors.black87,
                 ),
               ),
+
+              const Spacer(),
+
+              Text(
+                "Sign in to continue",
+                style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+              ),
+              SizedBox(height: 10),
+
+              Card(
+                elevation: 6,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                shadowColor: Colors.red,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 14),
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.g_mobiledata, color: Colors.black, size: 36),
+                      // Padding(
+                      //   padding: const EdgeInsets.all(8.0),
+                      //   child: Image.asset(
+                      //     "assets/images/signinwithgoogle.png", // Use clean icon only
+                      //     height: 28,
+                      //   ),
+                      // ),
+                      const SizedBox(width: 5),
+                      const Text(
+                        "Sign in with Google",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
             ],
           ),
         ),
