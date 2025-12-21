914c035955f1aea062e0f350f81cf519666bea8f
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-21 21:51:03 +0545

    ADD: Dependency- flutter_riverpod, firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:devanasoft_app/firebase_options.dart';
 import 'package:devanasoft_app/landing_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
