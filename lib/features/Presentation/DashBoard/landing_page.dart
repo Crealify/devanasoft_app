3ab34e036389d61da49933627dc9afa0eab7ec57
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-22 07:01:43 +0545

    UI: Landing Page

--- lib/features/Presentation/DashBoard/landing_page.dart
+++ lib/features/Presentation/DashBoard/landing_page.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
-  State<LandingPage> createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 120,
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Image.asset('assets/images/devanasoft_logos.png'),
+        ),
+
+        actions: const [
+          Icon(Icons.menu, color: Colors.blueAccent, size: 40),
+          SizedBox(width: 12),
+        ],
+      ),
+      body: SingleChildScrollView(child: Column()),
+    );
   }
 }
