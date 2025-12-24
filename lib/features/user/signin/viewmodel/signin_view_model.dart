1f4b2f6f1e0f2d2fa8ad8ed2876b2d9b4511bf6f
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-24 11:26:20 +0545

    Refector: otp verification UI

--- lib/features/user/signin/viewmodel/signin_view_model.dart
+++ lib/features/user/signin/viewmodel/signin_view_model.dart
@@ -14,27 +14,6 @@ class SigninViewModel extends ChangeNotifier {
     Navigator.pushNamed(context, route);
   }
 
-  // Future<void> signIn(BuildContext context) async {
-  //   if (!formKey.currentState!.validate()) return;
-  //   isLoading = true;
-  //   notifyListeners();
-  //   await Future.delayed(const Duration(seconds: 2));
-
-  //   final model = SigninModel(phoneNumber: phoneController.text);
-
-  //   print(' Sign-in Data: ${model.toJson()}');
-
-  //   isLoading = false;
-  //   notifyListeners();
-
-  //   ScaffoldMessenger.of(context).showSnackBar(
-  //     const SnackBar(
-  //       content: Text('Sign-in successful! OTP sent to your phone.'),
-  //       backgroundColor: Colors.green,
-  //     ),
-  //   );
-  //   navigateToRoute(context, '/otp');
-  // }
   Future<void> signIn(BuildContext context) async {
     if (!formKey.currentState!.validate()) return;
 
@@ -53,7 +32,7 @@ class SigninViewModel extends ChangeNotifier {
       '/otp',
       arguments: {'otp': generatedOtp, 'phone': phoneController.text},
     );
-    print('Generated OTP: $generatedOtp for phone: ${phoneController.text}');
+    // print('Generated OTP: $generatedOtp for phone: ${phoneController.text}');
   }
 
   @override
